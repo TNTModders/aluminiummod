c27e753cd7a06095149b363463d6db7985c2cad8
Tom Kate <https://tomkate@bitbucket.org/tntmodders/project_fassion.git>
2017-11-18 21:36:12 +0900

    チュートリアル初級編①　アイテムの追加

---
+++ src/main/java/com/tntmodders/tutorial/AluminiumMod.java
@@ -0,0 +1,38 @@
+package com.tntmodders.tutorial;
+
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLConstructionEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+@Mod(modid = "aluminiummod", version = "1.0", name = "AluminiumMod")
+public class AluminiumMod {
+    public static final Item ALUMINIUM = new ItemAluminium();
+
+    @Mod.EventHandler
+    //この関数でMODファイル自体をイベントの発火先にする。
+    public void construct(FMLConstructionEvent event) {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    //アイテムを登録するイベント。 旧preinitのタイミングで発火する。
+    @SubscribeEvent
+    public void registerItems(RegistryEvent.Register<Item> event) {
+        event.getRegistry().register(ALUMINIUM);
+    }
+
+    //モデルを登録するイベント。@SideOnlyによってクライアント側のみ呼ばれる。旧preinitのタイミングで発火する。
+    @SubscribeEvent
+    @SideOnly(Side.CLIENT)
+    public void registerModels(ModelRegistryEvent event) {
+        ModelLoader.setCustomModelResourceLocation(ALUMINIUM, 0, new ModelResourceLocation(new ResourceLocation("aluminiummod", "aluminium"), "inventory"));
+    }
+}
---
+++ src/main/java/com/tntmodders/tutorial/ItemAluminium.java
@@ -0,0 +1,17 @@
+package com.tntmodders.tutorial;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+public class ItemAluminium extends Item {
+
+    public ItemAluminium() {
+        super();
+        //レジストリに保存する名称を登録する。大文字禁止。
+        this.setRegistryName("aluminiummod", "aluminium");
+        //クリエイティブタブを設定する。
+        this.setCreativeTab(CreativeTabs.MATERIALS);
+        //翻訳名を登録する。大文字非推奨。
+        this.setUnlocalizedName("aluminium");
+    }
+}
